fe4955cf80a0ef6e4020d1e0b26bff6f3a2ff2d3
n1320447 <goveanestor@gmail.com>
2022-12-20 16:53:41 -0600

    adding main page first issue

--- FrontEnd/winterproj01/src/App.js
+++ FrontEnd/winterproj01/src/App.js
@@ -1,25 +1,53 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container, Row, Col, Button, Alert, Card, Form} from 'react-bootstrap'
+
+//export default class FormSubmission extends React.Component{
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div className="wrapper">
+    <h1>Winter Project</h1>
+    <form>
+    <fieldset>
+       <label>
+         <p>Name</p>
+         <input name="name" />
+       </label>
+     </fieldset>
+
+     <fieldset>
+       <label>
+         <p>Password</p>
+         <input name="name" />
+       </label>
+     </fieldset>
+     
+      <button type="submit" className="btn1">Submit</button>
+      <button type="submit" className="btn1">Create New User</button>
+     
+    </form>
+  </div>
+
+
+
+    // <div className = "App-main">
+    //   <div>
+    //       <p className ="App-header">Winter Project</p>
+    //   </div>
+
+    //   <div>
+    //   <form className ="App-userName">
+    //     <label for="fname"> User Name:</label><br/>
+    //     <input type="text" id = "fname" name="fname" value="John"></input><br/>
+    //   </form>
+    //   </div>
+    // </div>
   );
 }
+//}
 
 export default App;
